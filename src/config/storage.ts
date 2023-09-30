9d8806f5a072c41f741cb208d5d7f0c7651ddef4
soohyeok8901 <soohyeok8901@gmail.com>
2023-09-30 23:58:17 +0900

    Implement Image upload to S3 with resizing

---
+++ src/config/storage.ts
@@ -0,0 +1,18 @@
+import AWS, { S3 } from "aws-sdk";
+import { S3Client } from '@aws-sdk/client-s3';
+
+// export const storage = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_KEY
+//   }
+// });
+
+export const storage: AWS.S3 = new S3();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: "ap-northeast-2",
+});
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,82 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import sharp from 'sharp';
+import { v4 } from 'uuid'
+import { storage } from './config/storage';
+import { upload } from './middlewares/multer';
+
+// import { createServer } from "http";
+
+dotenv.config();
 
 const app: Express = express();
-const port = 3001;
+const port = process.env.PORT;
+// const httpServer = createServer(app);
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ hi: "hi" })
-});
+const apiVersion = 'v1';  // API 버전
+const apiEndPoint = `/api/${apiVersion}`;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post(`${apiEndPoint}/avatars`, upload.array('avatars'), async (req, res) => {
+  try {
+    const resizedImageUrls = [];
+
+    for (const file of (req.files as Express.Multer.File[])) {
+      const resizedBuffer = await sharp(file.buffer)
+        .resize({ height: 800 })
+        .toBuffer();
+
+      const key = v4();
+
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `resized/${key}`,
+        Body: resizedBuffer,
+        ContentType: 'image',
+        ACL: 'public-read',
+      };
+
+      await storage.putObject(params, (_, __) => null).promise();
+
+      resizedImageUrls.push(`https://haze-dev-8901.s3.ap-northeast-2.amazonaws.com/resized/${key}`);
+    }
+
+    res.json({ result: 'success', urls: resizedImageUrls });
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ result: 'error', error })
+  }
+})
+
+app.post(`${apiEndPoint}/avatar`, upload.single('avatar'), async (req, res) => {
+  try {
+    const resizedBuffer = await sharp(req.file.buffer)
+      .resize({ height: 800 })
+      .toBuffer();
+
+    const key = v4();
+
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: `resized/${key}`,
+      Body: resizedBuffer,
+      ContentType: 'image',
+      ACL: 'public-read',
+    };
+
+    await storage.putObject(params, (_, __) => null).promise();
+
+    res.json({ result: "success", url: `https://haze-dev-8901.s3.ap-northeast-2.amazonaws.com/resized/${key}` });
+  } catch (error) {
+    res.status(500).json({ result: "error", error: error })
+  }
+})
 
 app.listen(port, () => {
-  console.log(`https://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
 
-/**
- * TODO
- */
