7eb3132d622e37babeb891362dc5797a792e3b5f
soohyeok8901 <soohyeok8901@gmail.com>
2023-10-02 04:59:41 +0900

    Apply jwt middleware to image router

--- src/routes/imageRoute.ts
+++ src/routes/imageRoute.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import upload from '../middlewares/multer';
 import ImageController from '../controllers/imageController';
+import { verifyToken } from '../middlewares/jwt';
 // import ImageService from '../services/imageService';
 
 const imageRouter = Router();
 
 // images 이미지 여러장 POST
-imageRouter.post('/', upload.array('images'), ImageController.uploadImages);
+imageRouter.post('/', verifyToken, upload.array('images'), ImageController.uploadImages);
 
 // image 이미지 1장 POST
-imageRouter.post('/single', upload.single('image'), ImageController.uploadImage);
+imageRouter.post('/single', verifyToken, upload.single('image'), ImageController.uploadImage);
 
 //TODO GET 이미지
 
