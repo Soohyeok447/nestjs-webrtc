1788b52d5546ec57dbf97add38c8608feb6079ff
soohyeok8901 <soohyeok8901@gmail.com>
2023-10-02 17:28:32 +0900

    Add db config

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+export function setMongoose() {
+  mongoose
+    .connect(process.env.DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: true,
+      dbName: process.env.DB_NAME,
+    } as ConnectOptions)
+    .then((db) => {
+      console.log("DB 연결됨");
+    })
+    .catch((err) => {
+      console.log("DB 연결 실패", err);
+    });
+}
