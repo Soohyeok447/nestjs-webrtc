a45f0a2ccc333b389e27850fadda41c9764ce325
soohyeok8901 <soohyeok8901@gmail.com>
2023-10-02 22:16:36 +0900

    Improve security

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express';
+import { setMongoose } from './config/db';
+import apiRouter from './routes/apiRoute';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import apiRouter from './routes/apiRoute';
-import { setMongoose } from './config/db';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -15,7 +17,18 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.disable('x-powered-by');
 
+const throttle = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  message: {
+    message: '최대 요청 횟수를 초과했습니다. 잠시후 다시 시도해주세요.'
+  }
+});
+
+app.use('/api', throttle);
 app.use(`/api`, apiRouter);
 
 app.listen(port, () => {
