9be2d20c2a85897223efd22df0a7458926016c41
soohyeok8901 <soohyeok8901@gmail.com>
2023-12-05 15:24:41 +0900

    Refactor eventParameters

--- src/services/socketManager.ts
+++ src/services/socketManager.ts
@@ -1,7 +1,11 @@
 import { Server, Socket } from 'socket.io';
 import MatchingService from './matchingService';
 import { MatchEvents } from '../constants';
-import { RespondToIntroduce } from '../types/eventParameters';
+import {
+  CancelMatching,
+  RespondToIntroduce,
+  StartMatching,
+} from '../types/eventParameters';
 
 class SocketManager {
   private io: Server;
@@ -16,7 +20,7 @@ class SocketManager {
       console.log(socket.id, '접속');
 
       this.setupMatchingListener(socket);
-      // this.setupWebRTCListener(socket);
+      this.setupWebRTCListener(socket);
 
       socket.on('disconnect', () => {
         console.log(socket.id, '접속 해제');
@@ -31,14 +35,17 @@ class SocketManager {
     socket.status = 'idle';
 
     // 소개매칭 대기 시작
-    socket.on(MatchEvents.START_MATCHING, async (userId) => {
+    socket.on(MatchEvents.START_MATCHING, async ({ userId }: StartMatching) => {
       MatchingService.startMatching({ socket, userId });
     });
 
     // 소개매칭 대기 취소
-    socket.on(MatchEvents.CANCEL_MATCHING, async (userId) => {
-      MatchingService.cancelMatching({ socket, userId });
-    });
+    socket.on(
+      MatchEvents.CANCEL_MATCHING,
+      async ({ userId }: CancelMatching) => {
+        MatchingService.cancelMatching({ socket, userId });
+      },
+    );
 
     // 소개매칭 phase에서 Accept 또는 Decline 처리
     socket.on(
@@ -64,6 +71,41 @@ class SocketManager {
         }
       },
     );
+  }
+
+  private setupWebRTCListener(socket: Socket) {
+    // 클라이언트가 offer 이벤트를 보낼 때
+    socket.on('offer', (data) => {
+      const partnerSocket = socket.partnerSocket;
+
+      // partnerSocket에게 offer 이벤트 전송
+      partnerSocket.emit('offer', {
+        offer: data.offer,
+        socketId: socket.id,
+      });
+    });
+
+    // 클라이언트가 answer 이벤트를 보낼 때
+    socket.on('answer', (data) => {
+      const partnerSocket = socket.partnerSocket;
+
+      // partnerSocket에게 answer 이벤트 전송
+      partnerSocket.emit('answer', {
+        answer: data.answer,
+        socketId: socket.id,
+      });
+    });
+
+    // 클라이언트가 ice-candidate 이벤트를 보낼 때
+    socket.on('ice-candidate', (data) => {
+      const partnerSocket = socket.partnerSocket;
+
+      // partnerSocket에게 ice-candidate 이벤트 전송
+      partnerSocket.emit('ice-candidate', {
+        candidate: data.candidate,
+        socketId: socket.id,
+      });
+    });
   }
 }
 
