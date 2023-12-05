169e6ddc02b621a85f494a68c759e6733caf6f14
soohyeok8901 <soohyeok8901@gmail.com>
2023-12-05 15:13:29 +0900

    Refactor about matchingService and Solve timeout bugs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,5 @@ export * from './location';
 export * from './gender';
 export * from './interest';
 export * from './purpose';
-export * from './socketEvents';
+export * from './matchEvents';
+export * from './webrtcEvents';
---
+++ src/types/eventParameters.ts
@@ -0,0 +1,4 @@
+export type RespondToIntroduce = {
+  userId: string;
+  response: 'accept' | 'decline';
+};
