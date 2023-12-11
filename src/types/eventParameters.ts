9b0ad768191d45e779f3637733762820583fbd9a
soohyeok8901 <soohyeok8901@gmail.com>
2023-12-12 08:06:47 +0900

    Implement webchat

--- src/types/eventParameters.ts
+++ src/types/eventParameters.ts
@@ -1,12 +1,30 @@
-export type RespondToIntroduce = {
+export type RespondToIntroduceEvent = {
   userId: string;
   response: 'accept' | 'decline';
 };
 
-export type StartMatching = {
+export type StartMatchingEvent = {
   userId: string;
 };
 
-export type CancelMatching = {
+export type CancelMatchingEvent = {
   userId: string;
+};
+
+export type OfferEvent = {
+  // offer: RTCSessionDescriptionInit;
+  offer: any;
+  roomName: string;
+};
+
+export type AnswerEvent = {
+  // answer: RTCSessionDescriptionInit;
+  answer: any;
+  roomName: string;
+};
+
+export type IceEvent = {
+  // ice: RTCIceCandidate;
+  ice: any;
+  roomName: string;
 };
